cc0fbfca076ba983e2c1c26d5101f70d25459d08
guesser <matheus_guesser@estudante.sesisenai.org.br>
2026-04-09 12:15:11 -0300

    add- javascript corrigido

--- script.js
+++ script.js
@@ -1,204 +1,141 @@
-const cpfcadastros = ["14959203034", "12345678910", "10987654321", "32165498780"];
-
-const formu = document.getElementById("formu");
-const moradia = document.getElementById("moradia");
-const sozinho = document.getElementById("sozinho");
-const peta = document.getElementById("peta");
-const podepet = document.getElementById("podepet");
-const quintal = document.getElementById("temquintal");
-const mophoras = document.getElementById("mophoras");
-const msgerro = document.getElementById("msgerro");
-const resultado = document.getElementById("resultado");
+let cpfcadastros = ["14959203034", "12345678910", "10987654321", "32165498780"];
 
 
-sozinho.addEventListener("input", function () {
-    if (Number(sozinho.value) > 8) {
-        mophoras.style.display = "block";
-    } else {
-        mophoras.style.display = "none";
-    }
-})
-
-moradia.addEventListener("change", function () {
-    if (moradia.value === "apartamento") {
-        podepet.style.display = "block";
-        quintal.style.display = "none";
-    } else if (moradia.value === "casa") {
-        quintal.style.display = "block";
-        podepet.style.display = "none";
-    } else {
-        podepet.style.display = "none";
-        quintal.style.display = "none";
-    }
-
-});
-
-peta.addEventListener("change", function () {
-    if (peta.value === "nao") {
-        alert("Você nunca teve pet antes, a ONG poderá realizar um acompanhamento");
-    }
+document.getElementById("formu").addEventListener("submit", function(e) {
+    e.preventDefault();
 
 
-});
-
-formu.addEventListener("submit", function (event) {
-
-    event.preventDefault();
-    msgerro.innerHTML = "";
-    resultado.innerHTML = "";
-
-    let erros = [];
-
-
-
-
-const nome = document.getElementById("nome").value.trim();
-const email = document.getElementById("email").value.trim();
-const telefone = document.getElementById("telefone").value.trim();
-const cpf = document.getElementById("cpf").value.trim().replace(/\D/g, "");
-const idade = Number(document.getElementById("idade").value);
-const cidade = document.getElementById("cidade").value;
-const tipomoradia = document.getElementById("moradia").value;
-const possuiquintal = document.getElementById("quintal").value;
-const podepetnamorada = document.getElementById("podepet").value;
-const temquintalodmora = document.getElementById("temquintal").value;
-const petantes = document.getElementById("peta").value;
-const horassozinho = Number(document.getElementById("sozinho").value);
-const justhorasso = document.getElementById("justhorasso").value.trim();
-const motivo = document.getElementById("motivo").value.trim().toLowerCase();
-const termo = document.getElementById("termo").checked;
-const financeiro = document.getElementById("financeiro").value;
-const decisaohj = document.getElementById("decisaohj").value;
-const espacoexterno = document.getElementById("espacoexterno").value;
-
+let nome = document.getElementById("nome").value;
+let email = document.getElementById("email").value;
+let telefone = document.getElementById("telefone").value;
+let cpf = document.getElementById("cpf").value;
+let idade = Number(document.getElementById("idade").value);
+let cidade = document.getElementById("cidade").value;
+let tipomoradia = document.getElementById("moradia").value;
+let possuiquintal = document.getElementById("quintal").value;
+let podepetnamorada = document.getElementById("podepet").value;
+let temquintalodmora = document.getElementById("temquintal").value;
+let petantes = document.getElementById("peta").value;
+let horassozinho = Number(document.getElementById("sozinho").value);
+let justhorasso = document.getElementById("justhorasso").value;
+let motivo = document.getElementById("motivo").value;
+let termo = document.getElementById("termo").checked;
+let financeiro = document.getElementById("financeiro").value;
+let decisaohj = document.getElementById("decisaohj").value;
 
 if (nome.length < 3) {
     alert("O nome deve ter pelo menos 3 caracteres.");
+    return false
 }
 
 if (!email.includes("@")) {
     alert("O email deve conter o caractere '@'.");
+     return false
 }
 
-const telefonenum = telefone.replace(/\D/g, "");
-if (telefonenum.length < 8) {
-    alert("O telefone deve ter no mínimo 8 dígitos.")
+if (telefone.length < 8) {
+    alert("O telefone deve ter no mínimo 8 dígitos.");
+     return false
 }
 
 if (cpf === "") {
     alert("O CPF é obrigatório");
+     return false
 }
 
 if (idade < 18 || isNaN(idade)) {
     alert("Sua idade deve ser maior ou igual a 18 anos");
+     return false
 }
 
 if (cidade === "") {
     alert("É obrigatório informar a cidade.");
+     return false
 }
 
 if (tipomoradia === "") {
     alert("É obrigatório informar o tipo de moradia.");
+     return false
 }
 
 if (possuiquintal === "") {
     alert("É necessário informar se você possui quintal");
+     return false
 }
 
 if (petantes === "") {
     alert("É necessário informar se você já teve algum pet");
+     return false
 }
 
 if (isNaN(horassozinho) || horassozinho < 0) {
     alert("Insira um valor numérico válido");
+     return false
 }
 
 if (motivo.length < 10) {
     alert("O motivo deve ter pelo menos 10 caracteres.");
-}
-
-if (!termo) {
-    alert("Você deve aceitar o termo de responsabilidade.");
-}
-
-if (idade < 18) {
-    alert("Você deve ter mais de 18 anos para adotar um pet.");
+     return false
 }
 
 if (tipomoradia === "apartamento" && podepetnamorada === "") {
     alert("Informe se o local permite animais:");
+     return false
 
 }
 
 if (tipomoradia === "casa" && temquintalodmora === "") {
     alert("Informa se o quintal é seguro:");
+     return false
 }
 
 if (horassozinho > 8) {
     alert("Problema: O animal ficará mais de 8 horas por dia sozinho no dia.");
     if (justhorasso.length < 5) {
         alert("Informe uma justificativa adicional e plausível.");
+         return false
     }
 }
 
 if (petantes === "nao") {
     alert("Poderá haver acompanhamento inicial por parte da ONG");
+
 }
 
-const motivosgen = ["quero", "porque sim", "quero um cachorro"];
+let motivosgen = ["quero", "porque sim", "quero um cachorro"];
 if (motivosgen.includes(motivo)) {
     alert("O motivo é muito genérico");
+     return false
 }
 
 if (cpfcadastros.includes(cpf)) {
     alert("O CPF já se encontra cadastrado, insira um CPF válido");
+     return false
 }
 
 if (!termo) {
     alert("Você deve aceitar o termo para poder enviar o formulário");
+     return false
 }
 
 if (tipomoradia === "apartamento" && possuiquintal === "sim") {
     alert("Não é possível indicar uso de espaço externo sem possuir quintal");
+     return false
 }
 
 if (financeiro === "nao") {
     alert("Envio do formulário cancelado por falta de condições financeiras");
+     return false
 }
 
 if (decisaohj === "sim") {
     alert("Reflita antes de adotar.");
+     return false
 }
 
-if (telefonenum.length < 10 || telefonenum.length > 11) {
-    alert("Informe um telefone válido.");
-}
+alert("Formulário enviado com sucesso!")
 
-if (erros.length > 0) {
-    msgerro.innerHTML = "<ul>" + erros.map(erro => `<li>${erro}</li>`).join("") + "</ul>";
-    return;
-}
 
-resultado.innerHTML = `
-    <h2>Formulário enviado!</h2>
-    <p><strong>Nome:</strong> ${nome}</p>
-    <p><strong>Email:</strong> ${email}</p>
-    <p><strong>Telefone:</strong> ${telefone}</p>
-    <p><strong>CPF:</strong> ${cpf}</p>
-    <p><strong>Idade:</strong> ${idade}</p>
-    <p><strong>Cidade:</strong> ${cidade}</p>
-    <p><strong>Moradia:</strong> ${tipomoradia}</p>
-    <p><strong>Tem quintal na moradia:</strong> ${possuiquintal}</p>
-    <p><strong>Já teve pet antes:</strong> ${petantes}</p>
-    <p><strong>Horas que fica sozinho:</strong> ${horassozinho}</p>
-    <p><strong>Motivo:</strong> ${motivo}</p>
 
-`;
-
-cpfcadastros.push(cpf);
-formu.reset();
-podepet.style.display = "none";
-quintal.style.display = "none";
-mophoras.style.display = "none";
 
 });
